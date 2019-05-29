6b0111824bb9592752841129c52f1225275a2235
trit <trit@quantumtrip.org>
2019-05-29 19:24:41 -0400

    Working on window

---
+++ src/window.rs
@@ -0,0 +1,18 @@
+extern crate gfx_hal as hal;
+
+#[cfg(feature = "gl")]
+{
+    extern crate gfx_backend_gl as back;
+    extern crate glutin as win;
+}
+
+struct Window {}
+
+impl Window
+{
+    
+}
+
+impl Default for Window {
+    
+}
